f44c3303044dcaec2e56df7b7e5911c3b0f08243
animepewdz <dkrishna987@gmail.com>
2024-01-10 20:56:13 +0530

    Updated with Validation and ejs support

---
+++ validation.js
@@ -0,0 +1,49 @@
+const { validationResult, body } = require("express-validator");
+
+//Validation Chain Middleware for Creating a Task
+let CreateTaskValid = [
+    //Validators
+    body("id").exists().withMessage("'id' field required"),
+    body("id").isInt().withMessage("'id' must be an integer"),
+    body("task").exists().withMessage("'task' field required"),
+    body("task").isString().withMessage("'task' must be a string"),
+    body("created").exists().withMessage("'created' field required"),
+    body("created")
+        .isISO8601()
+        .toDate()
+        .withMessage("'created' must be a date"),
+    //Sanitizer
+    body("task").trim().escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
+];
+
+//Validation Chain Middleware for Updating a Task
+let UpdateTaskValid = [
+    //Validators
+    body("id").exists().withMessage("'id' field required"),
+    body("id").isInt().withMessage("'id' must be an integer"),
+    body("completed").exists().withMessage("'completed' field required"),
+    body("completed")
+        .isIn([0, 1])
+        .withMessage("'completed' must be a boolean(0 or 1)"),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
+];
+
+module.exports = {
+    CreateTaskValid,
+    UpdateTaskValid,
+};
